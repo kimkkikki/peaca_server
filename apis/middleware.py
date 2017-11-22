fbfc35b96472973072e0ed2c56879a725af933a3
kimkkikki <kimkkikki1@gmail.com>
2017-11-22 14:25:29 +0900

    middleware bug fix

--- apis/middleware.py
+++ apis/middleware.py
@@ -12,28 +12,29 @@ def pre_handle_request(get_response):
     # One-time configuration and initialization.
 
     def middleware(request):
-        _dont_check_token = [
-            '/apis/user',
-        ]
-
         _meta = request.META
         _path = _meta['PATH_INFO']
 
-        if _path not in _dont_check_token:
-            try:
-                _uid = _meta['HTTP_ID']
-                _token = _meta['HTTP_TOKEN']
+        if 'apis'   in _path:
+            _dont_check_token = [
+                '/apis/user',
+            ]
 
+            if _path not in _dont_check_token:
                 try:
-                    _user = User.objects.get(id=_uid, token=_token)
-                    request.user = _user
-                except ObjectDoesNotExist:
-                    return HttpResponse(status=401)
-                except ValidationError:
-                    return HttpResponse(status=401)
+                    _uid = _meta['HTTP_ID']
+                    _token = _meta['HTTP_TOKEN']
 
-            except KeyError:
-                return HttpResponse(status=401)
+                    try:
+                        _user = User.objects.get(id=_uid, token=_token)
+                        request.user = _user
+                    except ObjectDoesNotExist:
+                        return HttpResponse(status=401)
+                    except ValidationError:
+                        return HttpResponse(status=401)
+
+                except KeyError:
+                    return HttpResponse(status=401)
 
         response = get_response(request)
 
